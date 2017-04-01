44a3b8e8e8bf0d130455260a4cdc1634278b522a
sevenysw <sevenysw@126.com>
2017-04-01 14:00:37 +0800

    Update softShrinkage.m

--- Decurtain/softShrinkage.m
+++ Decurtain/softShrinkage.m
@@ -1,3 +1,9 @@
+% Removal of curtaining effects by a variational model with directional forward differences
+% Author: Jan Henrik Fitschen
+% Reference: Jan Henrik Fitschen, Jianwei Ma, Sebastian Schuff, Removal of curtaining effects by a variational model with directional forward differences, Computer Vision and Image Understanding, Volume 155, February 2017, Pages 24-32, ISSN 1077-3142, http://dx.doi.org/10.1016/j.cviu.2016.12.008.
+% Email:  jma@hit.edu.cn
+% March 22, 2017
+
 function A = softShrinkage(A,lambda)
 %compute soft-shrinkage of A with treshold lambda
 
