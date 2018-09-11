b199a377c6c2bc44a28e57bde8ad75092b11a682
sevenysw <sevenysw@126.com>
2018-09-11 17:28:39 +0800

    remove old EMPCR

--- EMPCR/Hankel_2D.m
+++
@@ -1,8 +0,0 @@
-function M = Hankel_2D( df1,r )
-N=size(df1);
-n=N(2);
-C=df1(1:r);
-R=df1(r:n);
-M=hankel(C,R);
-end
-
--- EMPCR/SNR.m
+++
@@ -1,8 +0,0 @@
- function snr=SNR(I,In)
- % 计算信号噪声比函数
-% I: original signal
- % In: noisy signal(ie. original signal + noise signal)
- % snr=10*log(sigma2(I2)/sigma2(I2-I1))
- Ps=sum(sum(I.^2));%signal power
- Pn=sum(sum((In-I).^2));%noise power
- snr=10*log10(Ps/Pn);
